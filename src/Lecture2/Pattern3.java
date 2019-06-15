5c4d035570623e7e8dbfc80a671e8d8e08a22ed7
Ishan Agrawal <ishan38@outlook.com>
2019-06-15 20:55:29 +0530

    added pattern 3 code

--- src/Lecture2/Pattern3.java
+++ src/Lecture2/Pattern3.java
@@ -1,29 +1,30 @@
-//Print following pattern (range = 8)
-//        *
-//        **
-//        ***
-//        ****
-//        *****
-//        ******
-//        *******
-//        ********
+//Print following pattern (range = 5)
+/*
+        1
+        2	3
+        4	5	6
+        7	8	9	10
+        11	12	13	14	15
+*/
 
 package Lecture2;
 
 import java.util.Scanner;
 
-public class Pattern2 {
+public class Pattern3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("<--Enter range-->");
         int n = s.nextInt();
         int row = 1;
         int nst = 1;
+        int value = 1;
         while (row <= n) {
             int cst = 1;
             while (cst <= nst) {
-                System.out.print("*");
+                System.out.print(value+"\t");
                 cst++;
+                value++;
             }
             System.out.println();
             nst++;
