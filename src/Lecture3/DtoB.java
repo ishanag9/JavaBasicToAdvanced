89965887391a056b84440f9c94315d2ba7d1e637
Ishan Agrawal <ishan38@outlook.com>
2019-06-15 12:43:01 +0530

    added decimal to binary conversion program

---
+++ src/Lecture3/DtoB.java
@@ -0,0 +1,16 @@
+//Convert Decimal to Binary
+package Lecture3;
+
+public class DtoB {
+    public static void main(String[] args) {
+        int n = 45;
+        while(n != 1)
+        {
+            int m = n%2;
+            System.out.print(m);
+            n = n / 2;
+        }
+        System.out.print(1);
+
+    }
+}
