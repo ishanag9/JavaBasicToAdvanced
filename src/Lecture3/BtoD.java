3e1f45fcc3564fcd9168ea65c717c3adcf1a4539
Ishan Agrawal <ishan38@outlook.com>
2019-06-16 12:09:53 +0530

    added Convert binary number in decimal code

--- src/Lecture3/BtoD.java
+++ src/Lecture3/BtoD.java
@@ -1,6 +1,21 @@
+//Convert binary number in decimal
 package Lecture3;
+
+import java.util.Scanner;
 
 public class BtoD {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter binary number to convert in decimal");
+        int bin = s.nextInt();
+        int dec = 0;
+        int twopowers = 1;
+        while (bin != 0) {
+            int rem = bin % 10;
+            dec = dec + rem * twopowers;
+            twopowers = twopowers * 2;
+            bin = bin / 10;
+        }
+        System.out.println(dec);
     }
 }
