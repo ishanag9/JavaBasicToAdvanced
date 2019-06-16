53c06ba1717416acda2ae57d1b94268485754e27
Ishan Agrawal <ishan38@outlook.com>
2019-06-16 22:38:50 +0530

    added Delhi Odd Even code

---
+++ src/Lecture3/DelhiOddEven.java
@@ -0,0 +1,46 @@
+/*
+Due to growing Traffic Problem Kejriwal wants to devise a new scheme.
+The scheme is as follows, each car will be allowed to run on Sunday
+if the sum of digits which are even is divisible by 4
+or sum of digits which are odd in that number is divisible by 3.
+However to check every car for the above criteria can't be done by the Delhi Police.
+You need to help Delhi Police by finding out if a car numbered N will be allowed to run on Sunday?
+*/
+package Lecture3;
+
+import java.util.Scanner;
+
+public class DelhiOddEven {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter no. of vehicles");
+        int ncars = s.nextInt();
+        int sum = 0;
+        int i = 1;
+        while (i <= ncars) {
+            System.out.println("Enter unique vehicle number");
+            int n = s.nextInt();
+            while (n != 0) {
+                int rem = n % 10;
+                sum = sum + rem;
+                n = n / 10;
+            }
+            i++;
+            if (sum % 2 == 0) {
+                if (sum % 4 == 0) {
+                    System.out.println("Allowed");
+                } else {
+                    System.out.println("Not Allowed");
+                }
+            } else {
+                if (sum % 3 == 0) {
+                    System.out.println("Allowed");
+                } else {
+                    System.out.println("Not allowed");
+                }
+            }
+        }
+
+
+    }
+}
