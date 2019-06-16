59835e634988b1db2d3194de16e24504ccf8daaa
Ishan Agrawal <ishan38@outlook.com>
2019-06-16 23:31:02 +0530

    change in fahrenheit to celsius code

--- src/Lecture3/FtoC.java
+++ src/Lecture3/FtoC.java
@@ -1,5 +1,12 @@
-//Use of Datatypes
-//Convert fahrenheit to celsius
+////Use of Datatypes
+//Take the following as input.
+//
+//        Minimum Fahrenheit value
+//        Maximum Fahrenheit value
+//        Step
+//
+//        Print as output the Celsius conversions. Use the formula C = (5/9)(F – 32)
+
 package Lecture3;
 
 import java.util.Scanner;
@@ -7,11 +14,13 @@ import java.util.Scanner;
 public class FtoC {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-                int F = 0;
-                while(F<=300){
-                    int C = (int)((5.0/9) * (F - 32));
-                    System.out.println(F + "\t" + C);
-                    F = F + 20;
-                }
+        int minF = s.nextInt();
+        int maxF = s.nextInt();
+        int step = s.nextInt();
+        while (minF <= maxF) {
+            int C = (int) ((5.0 / 9) * (minF - 32));
+            System.out.println(minF + "\t" + C);
+            minF = minF + step;
+        }
     }
 }
