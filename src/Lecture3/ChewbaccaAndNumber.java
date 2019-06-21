620e7f13c2c992a95880a0c22c74bd37e81554a4
Ishan Agrawal <ishan38@outlook.com>
2019-06-21 14:41:40 +0530

    added ChewbaccaAndNumber code

--- src/Lecture3/ChewbaccaAndNumber.java
+++ src/Lecture3/ChewbaccaAndNumber.java
@@ -5,35 +5,21 @@ import java.util.Scanner;
 public class ChewbaccaAndNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter number");
-        long n = s.nextLong();
-        long rem1 = 0;
-        long rem2 = 0;
-        long count = 0;
-        while (n != 0) {
-            rem1 = n % 10;
-            n = n / 10;
-            count++;
-            {
-                while (n != 0) {
-                    rem2 = n % 10;
-                    n = n / 10;
-                    count++;
-                }
+        String str = s.nextLine();
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]=='9' && i == 0) {
+                i++;
             }
-        }
-        if (rem2 < rem1) {
-            while (count != 0) {
-                System.out.print(rem2);
-                count--;
+            int digit = arr[i] - '0';
+            if (digit >= 5) {
+                digit = 9 - digit;
             }
+            arr[i]= (char) (digit + '0');
         }
-
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
     }
 }
-/*
-        #1	run-error
-        #2	run-error
-        #3	no-output
-        #4	no-output
-        #5	wrong-answer*/
+
