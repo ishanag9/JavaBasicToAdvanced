f63f9d8611dd9bb161b59db07d707b776830d1e0
Ishan Agrawal <ishan38@outlook.com>
2019-06-22 22:13:35 +0530

    added Array Opeartions

--- src/Lecture4/ArrOps.java
+++ src/Lecture4/ArrOps.java
@@ -99,7 +99,7 @@ public class ArrOps {
     private static int max(int[] arr) {
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[0]) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
