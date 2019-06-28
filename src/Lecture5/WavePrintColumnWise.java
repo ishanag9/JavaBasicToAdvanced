7eec716fd0cd7d835c1c755c9df66ae578a0d2bc
Ishan Agrawal <ishan38@outlook.com>
2019-06-28 22:33:07 +0530

    added Wave Print ColumnWise program

--- src/Lecture5/WavePrintColumnWise.java
+++ src/Lecture5/WavePrintColumnWise.java
@@ -2,19 +2,19 @@ package Lecture5;
 
 import java.util.Scanner;
 
-public class TwoDArrayOps {
+public class WavePrintColumnWise {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         int n = s.nextInt();
         int[][] arr = new int[m][n];
-        input2Darr(arr, s, m, n);
-        waveprint(arr,m,n);
-//            System.out.println();
-//            spiralprint(arr);
+        input2Darr(arr, m, n, s);
+//        int[][] arr = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}, {41, 42, 43, 44}};
+        waveprintcolumn(arr, m, n);
+
     }
 
-    private static int[][] input2Darr(int[][] arr, Scanner s, int m, int n) {
+    private static int[][] input2Darr(int[][] arr, int m, int n, Scanner s) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = s.nextInt();
@@ -24,25 +24,23 @@ public class TwoDArrayOps {
         return arr;
     }
 
-//    private static void spiralprint(int[][] arr) {
-//
-//        }
-
-    private static void waveprint(int[][] arr,int m,int n) {
-
-        for (int row = 0; row < m; row++) {
-            if (row % 2 == 0) {
-                for (int col = 0; col < n; col++) {
+    private static void waveprintcolumn(int[][] arr, int m, int n) {
+        for (int col = 0; col < n; col++) {
+            if (col % 2 == 0) {
+                for (int row = 0; row < m; row++) {
                     System.out.print(arr[row][col] + ", ");
+
                 }
+
+
             } else {
-                for (int col = n - 1; col >= 0; col--) {
+                for (int row = m-1; row >= 0; row--) {
                     System.out.print(arr[row][col] + ", ");
+
                 }
             }
-//                System.out.println();
+
         }
         System.out.println("END");
     }
-
 }
