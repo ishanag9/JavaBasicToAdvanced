bb73fe6ba19438a3f0e0186fe8637943fe6f92b3
Ishan Agrawal <ishan38@outlook.com>
2019-07-02 21:46:45 +0530

    change in Shopping Game Program

--- src/Lecture1/ShoppingGame.java
+++ src/Lecture1/ShoppingGame.java
@@ -1,22 +1,27 @@
-package Lecture1;
-
-import java.util.Scanner;
-
-public class ShoppingGame {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter range");
-        int n = s.nextInt();
-        int i=0;
-        while (i < n) {
-            int n1 =s.nextInt();
-            int n2 =s.nextInt();
-            if (n1 > n2) {
-                System.out.println("Aayush");
-            }else {
-                System.out.println("Harshit");
-            }
-            i++;
-        }
-    }
-}
+//package Lecture1;
+//
+//import java.util.Scanner;
+//
+//public class ShoppingGame {
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter range");
+//        int n = s.nextInt();
+//        int i=0;
+//        while (i < n) {
+//            int n1 =s.nextInt();
+//            int n2 =s.nextInt();
+//            int sumAyush = 0;
+//            int sumHarshit = 0;
+//            while (1) {
+//
+//            }
+//            if (n1 > n2) {
+//                System.out.println("Aayush");
+//            }else {
+//                System.out.println("Harshit");
+//            }
+//            i++;
+//        }
+//    }
+//}
