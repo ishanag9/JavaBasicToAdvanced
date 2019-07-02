8ef6f49bdf4165118266f4982a991f53d6856527
Ishan Agrawal <ishan38@outlook.com>
2019-07-03 01:04:48 +0530

    added Next Smaller Of Next Greater Program

--- src/Lecture4/NextSOrGNumber.java
+++ src/Lecture4/NextSOrGNumber.java
@@ -1,3 +1,13 @@
+// Given array of integer, find the next smaller of next greater element of every element in array.
+
+//  Sample Input:
+//  1
+//  7
+//  5 1 9 2 5 1 7
+
+//  Sample Output:
+//  2 2 -1 1 -1 -1 -1
+
 package Lecture4;
 
 import java.util.Scanner;
@@ -5,22 +15,51 @@ import java.util.Scanner;
 public class NextSOrGNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int[] arr = {5, 1, 9, 2, 5, 1, 7};
-        nextNumber(arr);
-    }
-
-    private static void nextNumber(int[] arr) {
-        int i = 0, j = 0, k = 0;
-        while (i < arr.length && j < arr.length && k < arr.length) {
-            if (arr[i] < arr[j] && arr[j] > arr[k]) {
-                arr[i] = arr[k];
-            } else {
-                arr[i] = -1;
+        int t = s.nextInt();
+        while (t > 0) {
+            int n = s.nextInt();
+            int[] arr = new int[n];
+            arr = inputArr(arr, s);
+            t--;
+            for (int i = 0; i < arr.length; i++) {
+                int greaterIndex = nextGNumber(arr, i);
+                int smaller = nextSnumber(arr, greaterIndex);
+                System.out.print(smaller + " ");
             }
         }
-        for (int l = 0; l < arr.length; l++) {
-            System.out.print(arr[l]);
+//        int[] arr = {5, 1, 9, 2, 5, 1, 7};
 
+    }
+
+    private static int[] inputArr(int[] arr, Scanner s) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]  = s.nextInt();
         }
+        return arr;
+    }
+
+    private static int nextSnumber(int[] arr, int greaterIndex) {
+        if (greaterIndex == -1) {
+            return -1;
+        }
+        for (int i = greaterIndex; i <= greaterIndex; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[greaterIndex] > arr[j]) {
+                    return arr[j];
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static int nextGNumber(int[] arr, int index) {
+        for (int i = index; i <= index; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    return j;
+                }
+            }
+        }
+        return -1;
     }
 }
