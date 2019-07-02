6fecc758698397b9a2bde643b6a8bb234a429778
Ishan Agrawal <ishan38@outlook.com>
2019-07-02 21:45:40 +0530

    added Stack Using Arrays Client

---
+++ src/Lecture10/StackUsingArraysClient.java
@@ -0,0 +1,18 @@
+package Lecture10;
+
+public class StackUsingArraysClient {
+    public static void main(String[] args) throws Exception {
+        StackUsingArrays stack = new StackUsingArrays(5);
+        for (int i = 1; i <= 5; i++) {
+            stack.push(i * 10);
+            displayStackInfo(stack);
+        }
+    }
+
+    private static void displayStackInfo(StackUsingArrays stack) throws Exception{
+        stack.display();
+        System.out.println("TOP--> " + stack.top());
+        System.out.println("Size--> " + stack.size());
+        System.out.println();
+    }
+}
