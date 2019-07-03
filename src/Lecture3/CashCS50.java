79616abfab12306cc5fe881f2b370a629768e938
Ishan Agrawal <ishan38@outlook.com>
2019-07-03 15:07:48 +0530

    added Cash CS50 Program

---
+++ src/Lecture3/CashCS50.java
@@ -0,0 +1,34 @@
+package Lecture3;
+
+public class CashCS50 {
+    public static void main(String[] args) {
+        int change = 13;
+        int quarters = 25;
+        int dimes = 10;
+        int nickle = 5;
+        int penny = 1;
+        int coins = 0;
+        int sum = 0;
+        int remainingCoins = 0;
+
+        remainingCoins = change % quarters;
+        coins = change / quarters;
+        sum = sum + coins;
+        change = remainingCoins;
+
+        remainingCoins = change % dimes;
+        coins = change / dimes;
+        sum = sum + coins;
+        change = remainingCoins;
+
+        remainingCoins = change % nickle;
+        coins = change / nickle;
+        sum = sum + coins;
+        change = remainingCoins;
+
+        remainingCoins = change % penny;
+        coins = change / penny;
+        sum = sum + coins;
+        System.out.println(sum);
+    }
+}
