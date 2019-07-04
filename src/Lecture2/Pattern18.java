472c6ca58dedaf13efda54426672d36feb36b918
Ishan Agrawal <ishan38@outlook.com>
2019-07-05 02:06:44 +0530

    added Pattern18

--- src/Lecture2/Pattern18.java
+++ src/Lecture2/Pattern18.java
@@ -17,55 +17,51 @@ import java.util.Scanner;
 
 public class Pattern18 {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int row=1;
-        int nst=1;
-        int nsp=(2*n-1);
-        while(row<=(2*n+1)) {
-            int cst=1;
-            int value=n;
-            while(cst<=nst) {
-                System.out.print(value+"\t");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int row = 1;
+        int nst = 1;
+        int nsp = (2 * n - 1);
+        while (row <= (2 * n + 1)) {
+            int cst = 1;
+            int value = n;
+            while (cst <= nst) {
+                System.out.print(value + "\t");
                 cst++;
                 value--;
             }
-            int csp=1;
-            while(csp<=nsp) {
-                System.out.print(" "+"\t");
+            int csp = 1;
+            while (csp <= nsp) {
+                System.out.print(" " + "\t");
                 csp++;
-            }		int bst=1;
-            if(value==-1) {
-                value=value+2;
-                while(bst<row) {
-                    System.out.print(value+"\t");
+            }
+            int bst = 1;
+            if (value == -1) {
+                value = value + 2;
+                while (bst < row) {
+                    System.out.print(value + "\t");
                     value++;
                     bst++;
                 }
-            }
-
-            else {
-                value=value+1;
-                while(value<=n) {
-                    System.out.print(value+"\t");
+            } else {
+                value = value + 1;
+                while (value <= n) {
+                    System.out.print(value + "\t");
                     value++;
                 }
 
-
-
-
-            }if(row>=n+1) {
-                nst--;
-                nsp=nsp+2;
-
-            }else {
-                nst++;
-                nsp=nsp-2;
             }
+            if (row >= n + 1) {
+                nst--;
+                nsp = nsp + 2;
 
+            } else {
+                nst++;
+                nsp = nsp - 2;
+            }
 
             row++;
             System.out.println();
         }
     }
-    }
+}
