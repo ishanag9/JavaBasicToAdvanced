2e464d94cfe14ec27e122428be632368a459e44a
Ishan Agrawal <ishan38@outlook.com>
2019-07-04 23:03:55 +0530

    change in Spiral Print Anti-Clockwise program

--- src/Lecture5/SpiralPrintAntiClockwise.java
+++ src/Lecture5/SpiralPrintAntiClockwise.java
@@ -21,8 +21,15 @@ public class SpiralPrintAntiClockwise {
         int n = s.nextInt();
         int[][] arr = new int[m][n];
 //        int[][] arr = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}, {41, 42, 43, 44}};
-        input2Darr(arr, m, n, s);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = s.nextInt();
+
+            }
+        }
+//        input2Darr(arr, m, n, s);
         spiralPrint(arr);
+        System.out.println("END");
     }
 
     private static int[][] input2Darr(int[][] arr, int m, int n, Scanner s) {
@@ -37,7 +44,7 @@ public class SpiralPrintAntiClockwise {
 
     private static void spiralPrint(int[][] arr) {
         int top = 0, left = 0;
-        int bottom = arr[left].length - 1;
+        int bottom = arr.length - 1;
         int right = arr[top].length - 1;
         int dir = 1;
         int count = (bottom + 1) * (right + 1);
@@ -76,7 +83,7 @@ public class SpiralPrintAntiClockwise {
             if (count > 0) {
                 if (dir == 4) {
                     for (int i = right; i >= left; i--) {
-                        System.out.print(arr[top][i]+", ");
+                        System.out.print(arr[top][i] + ", ");
                         count--;
                     }
                 }
@@ -84,6 +91,6 @@ public class SpiralPrintAntiClockwise {
                 top++;
             }
         }
-        System.out.println("END");
+
     }
 }
