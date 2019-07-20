5d4d60312940088f6f26108e714b92c119a7e42b
Ishan Agrawal <ishan38@outlook.com>
2019-07-20 22:56:13 +0530

    change in Binary Tree Client

--- src/Lecture14/BinaryTreeClient.java
+++ src/Lecture14/BinaryTreeClient.java
@@ -3,6 +3,7 @@ package Lecture14;
 public class BinaryTreeClient {
     public static void main(String[] args) {
         // 50 true 25 true 12 false false true 81 false false true 75 true 48 false false true 16 false false
+        // 50 true 25 true 12 false false false true 75 true 62 false false false
         BinaryTree tree = new BinaryTree();
         tree.display();
 
